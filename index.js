b2c154ab8bbf5a718f0908d343109bf80ec3ba45
Krishnaraj004 <142392358+Krishnaraj004@users.noreply.github.com>
2024-08-01 20:53:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+function cal1() {
+   
+    const num1 = document.getElementById("ca").value;
+    const num2 = document.getElementById("caa").value;  
+    const result = document.getElementById("caaa");
+    const num = Number(num1) + Number(num2);
+    result.textContent =num
+  }
+  function cal2() {
+   
+    const num1 = document.getElementById("cc1").value;
+    const num2 = document.getElementById("cc2").value;  
+    const result = document.getElementById("cc3");
+    result.textContent = Number(num1) + Number(num2);
+    let s = Number(num1) + Number(num2)
+    sum(s)
+  }
+  function cal3() {
+   
+    const num1 = document.getElementById("jp1").value;
+    const num2 = document.getElementById("jp2").value;  
+    const result = document.getElementById("jp3");
+    result.textContent = Number(num1) + Number(num2);
+  }
+  function cal4() {
+   
+    const num1 = document.getElementById("pp1").value;
+    const num2 = document.getElementById("pp2").value;  
+    const result = document.getElementById("pp3");
+    result.textContent = Number(num1) + Number(num2);
+  }
+  function cal5() {
+   
+    const num1 = document.getElementById("se1").value;
+    const num2 = document.getElementById("se2").value;  
+    const result = document.getElementById("se3");
+    result.textContent = Number(num1) + Number(num2);
+  }
+  function cal6() {
+   
+    const num1 = document.getElementById("ai1").value;
+    const num2 = document.getElementById("ai2").value;  
+    const result = document.getElementById("ai3");
+    result.textContent = Number(num1) + Number(num2);
+  }
+  function cal7() {
+   
+    const num1 = document.getElementById("os1").value;
+    const num2 = document.getElementById("os2").value;  
+    const result = document.getElementById("os3");
+    result.innerHTML = Number(num1) + Number(num2);
+  }
+
+
+ 
